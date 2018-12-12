8eb292e06046d90362edea8d2cf41893662e39ba
Simon Hohl <simon.hohl@dainst.org>
2018-12-12 10:22:17 +0100

    Add Fixed-Length Data Elements to gazetteer authority data
    Also see https://www.loc.gov/marc/authority/concise/ad008.html.

--- harvesters/gazetteer_harvester.py
+++ harvesters/gazetteer_harvester.py
@@ -58,6 +58,31 @@ class GazetteerHarvester:
         field_001 = Field(tag='001', data=f"iDAI.gazetteer-{place['gazId']}")
         field_003 = Field(tag='003', data="DE-2553")
 
+        fixed_length_data_elements = datetime.date.today().isoformat().replace('-', '')  # index 5
+        fixed_length_data_elements += '|'  # 6
+        fixed_length_data_elements += '|'
+        fixed_length_data_elements += '|'
+        fixed_length_data_elements += '|'
+        fixed_length_data_elements += 'z'
+        fixed_length_data_elements += 'z'  # 11
+        fixed_length_data_elements += 'z'
+        fixed_length_data_elements += '|'
+        fixed_length_data_elements += '|'
+        fixed_length_data_elements += '|'
+        fixed_length_data_elements += '|'
+        fixed_length_data_elements += 'd'  # 17
+        fixed_length_data_elements += '          '  # 27
+        fixed_length_data_elements += '|'
+        fixed_length_data_elements += '|'
+        fixed_length_data_elements += ' '
+        fixed_length_data_elements += 'b'
+        fixed_length_data_elements += 'n'
+        fixed_length_data_elements += '|'
+        fixed_length_data_elements += '    '  # 37
+        fixed_length_data_elements += ' '
+        fixed_length_data_elements += ' '
+        field_008 = Field(tag='008', data=fixed_length_data_elements)
+
         field_024 = Field(
             tag=24, indicators=(7, ' '), subfields=[
                 'a', place['gazId'],
@@ -130,6 +155,7 @@ class GazetteerHarvester:
         record.leader = record.leader[0:6] + 'z' + record.leader[7:]
         record.add_field(field_001)
         record.add_field(field_003)
+        record.add_field(field_008)
         record.add_field(field_024)
         record.add_field(field_040)
 
